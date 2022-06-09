dd5ac7d01dd9e1a72fcf3d29d548a6e1dc5cf95e
jsdeemon <jsdeemon@gmail.com>
2022-06-09 06:35:08 +0500

    added input variables functionality

--- bot.py
+++ bot.py
@@ -6,12 +6,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-mail = "yourEmail@mail.com"
-key = "yourLinkedInPassword"
-message = "text of the message you wanna send" ## maximum 300 chars
-search_param = "Web developer javascript"
-pages_upto = 2
-totalCount = 3 ## Limit of invitations
+mail = input("Enter your LinkedIn Email: ")
+key = input("Enter your LinkedIn Password: ")
+search_param = input("Enter search phrase to find users: ")
+message = input("Enter message text (maximum 300 chars): ")
+pages_upto = input("Enter maximum pages number in result: ")
+totalCount = input("Enter maximum of sending requests: ")
 
 url = "https://www.linkedin.com"
 search_param = search_param.replace(" ", "%20")
@@ -31,7 +31,7 @@ submit = browser.find_element(By.XPATH, value="//button[@type='submit']")
 submit.click()
 time.sleep(random.randint(1, 5))
 
-for page in range(1, pages_upto):
+for page in range(1, int(pages_upto)):
     searcher_url = f"{url}/search/results/people/?geoUrn=%5B%22103644278%22%2C%22101165590%22%2C%22101174742%22%2C%22103350119%22%2C%22104305776%22%2C%22100456013%22%2C%22101452733%22%2C%22101620260%22%2C%22101728296%22%2C%22102105699%22%2C%22102264497%22%2C%22102890719%22%2C%22103819153%22%2C%22104170880%22%2C%22105072130%22%2C%22105117694%22%2C%22105490917%22%2C%22106670623%22%2C%22106693272%22%5D&keywords={search_param}&network=%5B%22S%22%5D&origin=FACETED_SEARCH&page={page}&serviceCategory=%5B%221275%22%2C%22669%22%2C%2269%22%2C%224044%22%2C%221723%22%2C%222004%22%2C%22482%22%2C%2210048%22%2C%225255%22%2C%221836%22%2C%222174%22%2C%22899%22%2C%2249%22%2C%222269%22%2C%22123%22%2C%22115%22%5D&sid=WRy"
 
     browser.get(searcher_url)
@@ -54,8 +54,9 @@ for page in range(1, pages_upto):
         browser.execute_script(browser_script, dismiss)
         time.sleep(random.randint(1, 15))
         counter += 1
-        if counter == totalCount:
+        if counter == int(totalCount):
             print("Messages & invitations sent: " + str(counter))
+            browser.quit()
             quit()
         if counter % 3 == 0:
             time.sleep(random.randint(1, 15))
